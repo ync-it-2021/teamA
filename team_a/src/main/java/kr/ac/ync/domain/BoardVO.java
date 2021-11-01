a3e5ba817969d059e373e4c168ff17285985618e
Administrator <Administrator@DESKTOP-N4S2IAG>
2021-11-01 09:15:56 +0900

    데이터베이스 변수 수정

--- team_a/src/main/java/kr/ac/ync/domain/BoardVO.java
+++ team_a/src/main/java/kr/ac/ync/domain/BoardVO.java
@@ -7,16 +7,34 @@ import lombok.Data;
 @Data
 public class BoardVO {
 
-	private Long bno;
-	private String title;
-	private String content;
-	private String writer;
-	private Date regdate;
-	private Date updateDate;
-	private int replyCnt;
-
-	private String file_1;
-	private String file_2;
-	private String file_3;
+//	private Long bno;
+//	private String title;
+//	private String content;
+//	private String writer;
+//	private Date regdate;
+//	private Date updateDate;
+//	private int replyCnt;
+//
+//	private String file_1;
+//	private String file_2;
+//	private String file_3;
+//	
+	
+	private Long bd_idx;
+	private String bd_code;
+	private String bd_title;
+	private String bd_contents;
+	private String member_id;
+	private Date bd_date;
+	private String bd_img1;
+	private String bd_img2;
+	private String bd_img3;
+	private String bd_img4;
+	private String bd_img5;
+	private char bd_del;
+	private int bd_cnt;
+	private int bd_comment_cnt;
+	private int bd_good_cnt;
+	private int bd_bad_cnt;
 	
 }
