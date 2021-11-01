1e56cbefa6139cfdd17f38e2bf028774b1317e54
Ohgytaek <Administrator@DESKTOP-N4S2IAG>
2021-11-01 11:14:28 +0900

    상품 출력 SQL문 추가

--- team_a/src/main/java/kr/ac/ync/service/ProductService.java
+++ team_a/src/main/java/kr/ac/ync/service/ProductService.java
@@ -1,5 +1,14 @@
 package kr.ac.ync.service;
 
+import java.util.List;
+
+import kr.ac.ync.domain.Criteria;
+import kr.ac.ync.domain.ProductVO;
+
 public interface ProductService {
+
+	public List<ProductVO> getListWithPaging(Criteria cri);
+	
+	public int getTotal(Criteria cri);
 
 }
--- team_a/src/main/java/kr/ac/ync/service/ProductServicempl.java
+++ team_a/src/main/java/kr/ac/ync/service/ProductServicempl.java
@@ -1,7 +1,11 @@
 package kr.ac.ync.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import kr.ac.ync.domain.Criteria;
+import kr.ac.ync.domain.ProductVO;
 import kr.ac.ync.mapper.ProductMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -12,5 +16,19 @@ import lombok.extern.log4j.Log4j;
 public class ProductServicempl implements ProductService {
 	
 	private ProductMapper mapper;
+
+	@Override
+	public List<ProductVO> getListWithPaging(Criteria cri) {
+		
+		log.info("get List with criteria: " + cri);
+
+		return mapper.getListWithPaging(cri);
+	}
+
+	@Override
+	public int getTotal(Criteria cri) {
+		log.info("get total count");
+		return mapper.getTotalCount(cri);
+	}
 
 }
