166d4b630aa7e499d0f424cd5373f307b975deb6
Administrator <Administrator@DESKTOP-N4S2IAG>
2021-11-01 11:12:41 +0900

    상품 출력, 페이징 메소드 추가

--- team_a/src/main/java/kr/ac/ync/mapper/ProductMapper.java
+++ team_a/src/main/java/kr/ac/ync/mapper/ProductMapper.java
@@ -1,5 +1,16 @@
 package kr.ac.ync.mapper;
 
+import java.util.List;
+
+import kr.ac.ync.domain.Criteria;
+import kr.ac.ync.domain.ProductVO;
+
 public interface ProductMapper {
+
+	public List<ProductVO> getListWithPaging(Criteria cri);
+	
+	public int getTotal(Criteria cri);
+	
+	public int getTotalCount(Criteria cri);
 
 }
