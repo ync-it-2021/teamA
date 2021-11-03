0d6cf23c4d3c0701af54c62131fa9ea5a386990b
gytaek <skele838@gmail.com>
2021-11-03 10:18:18 +0900

    주석추가

--- team_a/src/main/java/kr/ac/ync/mapper/ReviewMapper.java
+++ team_a/src/main/java/kr/ac/ync/mapper/ReviewMapper.java
@@ -8,16 +8,17 @@ import kr.ac.ync.domain.Criteria;
 import kr.ac.ync.domain.ReviewVO;
 
 public interface ReviewMapper {
-
+	
+	//리뷰 작성
 	public int insert(ReviewVO vo);
-
+	//리뷰 상세보기
 	public ReviewVO read(int prd_idx);
-
+	//리뷰 삭제
 	public int delete(int prd_idx);
-
+	//리뷰 수정
 	public int update(ReviewVO Review);
-
+	//
 	public List<ReviewVO> getListWithPaging(@Param("cri") Criteria cri, @Param("prd_idx") int prd_idx);
-
+	//
 	public int getCountByProduct(int prd_idx);
 }
