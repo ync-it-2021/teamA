fa15f2b80f68d44d86e374b3028553016ccd7b79
gytaek <skele838@gmail.com>
2021-11-03 14:56:08 +0900

    index페이지 controller,mapper,service 파일 생성

---
+++ team_a/src/main/java/kr/ac/ync/controller/IndexController.java
@@ -0,0 +1,35 @@
+package kr.ac.ync.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.ac.ync.domain.Criteria;
+import kr.ac.ync.domain.PageDTO;
+import kr.ac.ync.service.ProductService;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/front/*")
+public class IndexController {
+
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping("/index/")
+	public void test(Model model) {
+		
+		model.addAttribute("test", service.imgTest(1));
+		
+	}
+	
+
+
+
+}
