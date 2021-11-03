048ab9ffb2e940affd2ac8dae3befe168b6d73ec
gytaek <skele838@gmail.com>
2021-11-03 15:08:58 +0900

    글 등록 매소드 추가

--- team_a/src/main/java/kr/ac/ync/service/ProductService.java
+++ team_a/src/main/java/kr/ac/ync/service/ProductService.java
@@ -10,5 +10,10 @@ public interface ProductService {
 	public List<ProductVO> getListWithPaging(Criteria cri);
 	
 	public int getTotal(Criteria cri);
+	
+	// 글 등록
+	public void register(ProductVO prd);
+	
+	public ProductVO imgTest(int prd_idx);
 
 }
