78b9d179beea4bff2bb1a3487a0f22ee6ca8a59f
gytaek <skele838@gmail.com>
2021-11-10 09:18:25 +0900

    컬럼명과 어드민 아이디 추가, 상세보기, 검색(수정중) SQL문 메소드 추가

--- team_a/src/main/java/kr/ac/ync/mapper/MemberMapper.java
+++ team_a/src/main/java/kr/ac/ync/mapper/MemberMapper.java
@@ -1,8 +1,20 @@
 package kr.ac.ync.mapper;
 
+import java.util.List;
+
+import kr.ac.ync.domain.BoardVO;
+import kr.ac.ync.domain.Criteria;
 import kr.ac.ync.domain.MemberVO;
 
 public interface MemberMapper {
 
-	public MemberVO read(String userid);
+	public MemberVO read(String member_id);
+	
+	public List<MemberVO> getList();
+
+	public List<MemberVO> getListWithPaging(Criteria cri);
+	
+	public int getTotal(Criteria cri);
+	
+	public Integer insertSelectKey(MemberVO member);
 }
