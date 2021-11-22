061ad2a205c4074e3619861ce8743578e2f0aa21
gytaek <skele838@gmail.com>
2021-11-22 17:35:26 +0900

    index 페이지 이벤트 정보 출력

--- team_a/src/main/java/kr/ac/ync/controller/IndexController.java
+++ team_a/src/main/java/kr/ac/ync/controller/IndexController.java
@@ -4,34 +4,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.ac.ync.service.EventService;
 import kr.ac.ync.service.ProductService;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 @RequestMapping
-public class FrontController {
+public class IndexController {
 
 	
 	@Autowired
-	private ProductService service;
-
+	private ProductService prdService;
+	
+	@Autowired
+	private EventService evtService;
 	
 	@GetMapping("/")
 	public String index(Model model) {
 		
+		log.info("data: " + evtService.getList());
+		
+		model.addAttribute("evt",evtService.getList());
 		return"index";
 	}
-	//해더 이미지 호출
-	@GetMapping("/includes/header/")
-	public void linkTest(Model model) {
+	
+	@GetMapping("banner")
+	public void banner(Model model) {
+
 	}
 	
-	@GetMapping("/login/")
-	public void test1(Model model) {
-	}
+
+
+	
 }
