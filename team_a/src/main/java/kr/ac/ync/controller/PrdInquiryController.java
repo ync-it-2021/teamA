eb06d61a05a4b2417498a4deab008672c6fbdc44
gytaek <skele838@gmail.com>
2021-12-01 16:53:09 +0900

    상품문의 상품 get페이지 하단 review출력 창에 추가

--- team_a/src/main/java/kr/ac/ync/controller/PrdInquiryController.java
+++ team_a/src/main/java/kr/ac/ync/controller/PrdInquiryController.java
@@ -1,30 +1,40 @@
 package kr.ac.ync.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.ac.ync.domain.Criteria;
-import kr.ac.ync.domain.ProductVO;
+import kr.ac.ync.domain.PrdInquiryVO;
 import kr.ac.ync.service.PrdInquiryService;
 import lombok.extern.log4j.Log4j;
 
-@Controller
+@RestController
 @Log4j
-@RequestMapping({"/prdInuiry/*","/admin/prdInuiry/*"})
+@RequestMapping("/prdInquiry/*")
 public class PrdInquiryController {
 	
 	@Autowired
 	private PrdInquiryService service;
 	
-	@PostMapping( "/remove" )
-	public String delete(ProductVO prd, @ModelAttribute("cri") Criteria cri) {
+	@GetMapping(value = "/pages/{prd}/{page}", produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE })
+public ResponseEntity<List<PrdInquiryVO>> getList(@PathVariable("page") int page, @PathVariable("prd") int prd) {
 
-		log.info("/remove");
-		this.service.remove(prd.getPrd_idx());
-		return "redirect:/admin/product/list";
-	}
+log.info("getList.................");
+Criteria cri = new Criteria(page,10);
+log.info(cri);
+log.info(prd);
+log.info(service.getList(cri,prd));
 
+return new ResponseEntity<>(service.getList(cri,prd), HttpStatus.OK);
+}
+	
 }
