f38da6bc99228d6097ee1b9d81c3fa445dbfc6fb
gytaek <skele838@gmail.com>
2021-12-01 16:55:58 +0900

    admin메인 페이지 추가 *페이지에 들어갈거* 1.답장하지 않은 상품문의 2.답장하지 않은 1대1 문의 3.결제 확인중인 주문

---
+++ team_a/src/main/java/kr/ac/ync/controller/adminController.java
@@ -0,0 +1,20 @@
+package kr.ac.ync.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/admin")
+public class adminController {
+
+	
+	@GetMapping("")
+	public String index() {
+		
+		return "/admin/index";
+	}
+}
