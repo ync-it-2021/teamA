dd51cc614b069856b3f769d6bf80470fdfd56e8f
gytaek <skele838@gmail.com>
2021-12-09 17:34:24 +0900

    오류시 이동 URI 수정

--- team_a/src/main/java/kr/ac/ync/controller/CommonController.java
+++ team_a/src/main/java/kr/ac/ync/controller/CommonController.java
@@ -21,18 +21,21 @@ public class CommonController {
 	}
 
 	@GetMapping({"/customLogin","/login"})
-	public void loginInput(String error, String logout, Model model) {
+	public String loginInput(String error, String logout, Model model) {
 
 		log.info("error: " + error);
 		log.info("logout: " + logout);
 
 		if (error != null) {
 			model.addAttribute("error", "Login Error Check Your Account");
+			return null;
 		}
 
 		if (logout != null) {
 			model.addAttribute("logout", "Logout!!");
+			return "index";
 		}
+		return null;
 	}
 
 	@GetMapping("/customLogout")
