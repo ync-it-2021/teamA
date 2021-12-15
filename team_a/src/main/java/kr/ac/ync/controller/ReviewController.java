797a238c062b51f182a71273c097de0ad5969df7
gytaek <skele838@gmail.com>
2021-12-15 16:28:27 +0900

    필요없는 주석 추가 및 설명 주석 추가

--- team_a/src/main/java/kr/ac/ync/controller/ReviewController.java
+++ team_a/src/main/java/kr/ac/ync/controller/ReviewController.java
@@ -3,6 +3,7 @@ package kr.ac.ync.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +16,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.ac.ync.domain.Criteria;
 import kr.ac.ync.domain.ReviewVO;
 import kr.ac.ync.service.ReviewService;
+import kr.ac.ync.util.UploadUtils;
 import lombok.extern.log4j.Log4j;
 
 
@@ -26,41 +29,31 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @RequestMapping("/replies/*")
 public class ReviewController {
+	@Value("${globalConfig.uploadPath}")
+	private String uploadPath;
 	
 	@Autowired
 	private ReviewService service;
 	
-	// consumes은 호출하는쪽에서 application/json 요청만 받아들인다. 요청 컨텐트 타입 제한
-	// produces은 조건에 지정한 미디어 타입과 관련된 응답을 생성. 응답 컨텐트 타입 제한
-	// 명시적으로 consumes와 produces 조건을 각각 사용하는 것을 권장한다
-//	@PreAuthorize("isAuthenticated()")
-//	@PostMapping(value = "/new", consumes = "application/json", produces = { MediaType.TEXT_PLAIN_VALUE })
-//	public ResponseEntity<String> create(@RequestBody ReviewVO vo) {
-//
-//		log.info("ReviewVO: " + vo);
-//
-//		int insertCount = service.register(vo);
-//
-//		log.info("Review INSERT COUNT: " + insertCount);
-//
-//		return insertCount == 1
-//				? new ResponseEntity<>("success", HttpStatus.OK)
-//				: new ResponseEntity<>("Fail!! 댓글 등록 중 오류 발생",HttpStatus.INTERNAL_SERVER_ERROR);
-//	}
-	
-	// 교재와 다른 부분 내용 추가!
-	// spring 5.2에서 MediaType.APPLICATION_JSON_UTF8_VALUE 는 제거됨
-	// 해당 값 없어도 현재 브라우저는 UTF-8을 제대로 처리함.
-	// spring 5.2 부터 MediaType.APPLICATION_JSON_UTF8 로 수정하면됨
-	@GetMapping(value = "/{rv}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<ReviewVO> get(@PathVariable("rv") int rv) {
+	// 리뷰 생성 /로그인 상태
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping(value = "/new", consumes = "application/json", produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> create(@RequestBody ReviewVO vo,MultipartFile[] uploadFile) {
+		
+		vo.setReview_img(UploadUtils.uploadFormPost(uploadFile[0], uploadPath));
 
-		log.info("get: " + rv);
+		log.info("ReviewVO: " + vo);
 
-		return new ResponseEntity<>(service.get(rv), HttpStatus.OK);
+		int insertCount = service.register(vo);
+
+		log.info("Review INSERT COUNT: " + insertCount);
+
+		return insertCount == 1
+				? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>("Fail!! 댓글 등록 중 오류 발생",HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
-//	@PreAuthorize("principal.username == #vo.replyer")
+	// 리뷰 수정 /로그인 상태/ 본인만 수정 가능
+	@PreAuthorize("principal.username == #vo.member_id")
 	@RequestMapping(value = "/{rv}", method = { RequestMethod.PUT, RequestMethod.PATCH },
 					consumes = "application/json", produces = {	MediaType.TEXT_PLAIN_VALUE })
 	public ResponseEntity<String> modify(@RequestBody ReviewVO vo, @PathVariable("rv") int rv) {
@@ -76,8 +69,8 @@ public class ReviewController {
 				: new ResponseEntity<>("Fail!! 댓글 수정 중 오류 발생", HttpStatus.INTERNAL_SERVER_ERROR);
 
 	}
-
-//	@PreAuthorize("principal.username == #vo.replyer")
+	//리뷰 삭제 /로그인 상태 / 본인만 삭제 가능
+	@PreAuthorize("principal.username == #vo.member_id")
 	@DeleteMapping(value = "/{rv}", produces = { MediaType.TEXT_PLAIN_VALUE })
 	public ResponseEntity<String> remove(@RequestBody ReviewVO vo, @PathVariable("rv") int rv) {
 
@@ -89,7 +82,7 @@ public class ReviewController {
 				: new ResponseEntity<>("Fail!! 댓글 삭제 중 오류 발생",HttpStatus.INTERNAL_SERVER_ERROR);
 
 	}
-	
+	//리뷰 페이징
 	@GetMapping(value = "/pages/{prd}/{page}", produces = { MediaType.APPLICATION_XML_VALUE,
 															MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<List<ReviewVO>> getList(@PathVariable("page") int page, @PathVariable("prd") int prd) {
@@ -97,21 +90,21 @@ public class ReviewController {
 		 log.info("getList.................");
 		 Criteria cri = new Criteria(page,10);
 		 log.info(cri);
-		 log.info(prd);
+		 log.info("search id(product) : "+prd);
 		 log.info(service.getList(cri,prd));
 	
 		 return new ResponseEntity<>(service.getList(cri,prd), HttpStatus.OK);
 	 }
 	
-	
+	//관리자 페이지 회원 작성 리뷰 페이징
 	@GetMapping(value = "/member/pages/{id}/{page}", produces = { MediaType.APPLICATION_XML_VALUE,
-			MediaType.APPLICATION_JSON_VALUE })
+				MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<List<ReviewVO>> getList(@PathVariable("page") int page, @PathVariable("id") String member_id) {
 	
 	log.info("getList.................");
 	Criteria cri = new Criteria(page,10);
 	log.info(cri);
-	log.info(member_id);
+	log.info("search id(member) : "+member_id);
 	log.info(service.getList(cri,member_id));
 	
 	return new ResponseEntity<>(service.getList(cri,member_id), HttpStatus.OK);
